4a7bc3ea3db594a5f314165f0f7ce61e14e705c2
Ethan Gunter <100790052+EthanGunter@users.noreply.github.com>
2025-11-22 21:01:08 -0500

    Fix: md renderer not updating content + debounced updates

--- src/lib/debounce.ts
+++ src/lib/debounce.ts
@@ -1,14 +1,14 @@
 export default function debounce<T extends (...args: any[]) => any>(
     func: T,
     delay: number
-): (...args: Parameters<T>) => Promise<ReturnType<T>> {
+): (...args: Parameters<T>) => Promise<Awaited<ReturnType<T>>> {
     let timeoutId: NodeJS.Timeout | undefined = undefined;
     
-    return function (...args: Parameters<T>): Promise<ReturnType<T>> {
+    return function (...args: Parameters<T>): Promise<Awaited<ReturnType<T>>> {
         return new Promise((resolve) => {
             clearTimeout(timeoutId);
-            timeoutId = setTimeout(() => {
-                resolve(func(...args));
+            timeoutId = setTimeout(async () => {
+                resolve(await func(...args));
             }, delay);
         });
     };
